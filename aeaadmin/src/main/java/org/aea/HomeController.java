b89f48219886543448bd85c56a86b0ee93a4b561
Prasad Krishnan <pkrishnan@applieddatafinance.com>
2015-11-29 00:02:40 +0530

    UI Chnages

--- aeaadmin/src/main/java/org/aea/HomeController.java
+++ aeaadmin/src/main/java/org/aea/HomeController.java
@@ -5,25 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
   
   @Autowired
   public HomeService homeService;
-
-  @RequestMapping(value = "/greeting", method = RequestMethod.GET)
-  public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-      model.addAttribute("name", name);
-      homeService.test();
-      return "sample";
-  }
   
   @RequestMapping("/home")
   public String index(Model model) {
-      return "index";
+      return "user-registration";
   }
   
 }
