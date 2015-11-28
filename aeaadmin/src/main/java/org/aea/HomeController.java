d4211201ae3d86c39551202a73e53b657631c6de
vrenjit <vrenjit@applieddatafinance.com>
2015-11-28 19:10:20 +0530

    Controller setup

---
+++ aeaadmin/src/main/java/org/aea/HomeController.java
@@ -0,0 +1,28 @@
+package org.aea;
+
+import org.aea.service.HomeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+  
+  @Autowired
+  public HomeService homeService;
+
+  @RequestMapping("/greeting")
+  public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+      model.addAttribute("name", name);
+      homeService.test();
+      return "greeting";
+  }
+  
+  @RequestMapping("/ind")
+  public String index(Model model) {
+      return "index";
+  }
+  
+}
